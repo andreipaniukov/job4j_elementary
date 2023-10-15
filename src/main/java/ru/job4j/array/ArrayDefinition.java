43f8a1852fa4fac36e9cdc1acd320cd13354071b
Andrei Paniukov <jap21@yandex.ru>
2023-10-15 23:21:33 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentiev";
+        names[1] = "ivan Ivanov";
+        names[2] = "Petr Petrov";
+        names[3] = "Sidor Sidorov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
